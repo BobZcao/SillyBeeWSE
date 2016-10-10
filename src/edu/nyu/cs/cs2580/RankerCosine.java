ea086a77673da296c53ad7ca31e8693e7954d73b
ryekorn <handsomekaka0@gmail.com>
2016-10-09 22:16:21 -0400

    add vsm and numviews for ranker signals.

--- src/edu/nyu/cs/cs2580/RankerCosine.java
+++ src/edu/nyu/cs/cs2580/RankerCosine.java
@@ -1,6 +1,9 @@
 package edu.nyu.cs.cs2580;
 
 import java.util.Vector;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import edu.nyu.cs.cs2580.QueryHandler.CgiArguments;
 import edu.nyu.cs.cs2580.SearchEngine.Options;
@@ -13,16 +16,88 @@ import edu.nyu.cs.cs2580.SearchEngine.Options;
  */
 public class RankerCosine extends Ranker {
 
+  private Vector<Map<String, Double>> _docVectorSet = new Vector<Map<String, Double>>();
+
   public RankerCosine(Options options,
       CgiArguments arguments, Indexer indexer) {
     super(options, arguments, indexer);
+    loadDocumentVector();
     System.out.println("Using Ranker: " + this.getClass().getSimpleName());
+  }
+
+  void loadDocumentVector() {
+    for (int i = 0; i < _indexer.numDocs(); ++i) {
+      // Get l2-normalized tf-idf vector of document
+      Map<String, Double> docVector = new HashMap<String, Double>();
+      getDocumentVector(i, docVector);
+      _docVectorSet.add(docVector);
+    }
+  }
+
+  void getDocumentVector(int docid, Map<String, Double> docVector) {
+    Document doc = _indexer.getDoc(docid);
+    Vector<String> docTokens = ((DocumentFull) doc).getConvertedBodyTokens();
+    
+    // Record tokens frequency in document body
+    for (String token : docTokens) {
+      if (docVector.containsKey(token)) {
+        docVector.put(token, docVector.get(token) + 1.0);
+      } else {
+        docVector.put(token, 1.0);
+      }
+    } 
+
+    // Compute tf-idf vector
+    double normalizeFactor = 0.0;
+    for (Map.Entry<String, Double> entry : docVector.entrySet()) {
+      double IDF = getTermIDF(entry.getKey());
+      normalizeFactor += Math.pow((entry.getValue() * IDF), 2);
+      entry.setValue(entry.getValue() * IDF);
+    }
+    normalizeFactor = Math.sqrt(normalizeFactor);
+
+    //L2-Normalize
+    for (Map.Entry<String, Double> entry : docVector.entrySet()) {      
+      entry.setValue(entry.getValue() / normalizeFactor);      
+    }    
   }
 
   @Override
   public Vector<ScoredDocument> runQuery(Query query, int numResults) {
+    
+    // Process the raw query into tokens.
+    query.processQuery();
+
     Vector<ScoredDocument> all = new Vector<ScoredDocument>();
-    // @CS2580: fill in your code here.
-    return all;
+
+    for (int i = 0; i < _indexer.numDocs(); ++i) {
+      // Get l2-normalized tf-idf vector of document
+      Map<String, Double> docVector = _docVectorSet.get(i);
+
+      // Compute score for document
+      // Present query vector as tf-vector
+      double score = 0.0;
+      for(String queryToken : query._tokens) {
+        if (docVector.containsKey(queryToken)) {
+          score += docVector.get(queryToken) * 1.0;
+        }
+      }
+      all.add(new ScoredDocument(query._query, _indexer.getDoc(i), score));
+    }
+
+    Collections.sort(all, Collections.reverseOrder());
+    Vector<ScoredDocument> results = new Vector<ScoredDocument>();
+    for (int i = 0; i < all.size() && i < numResults; ++i) {
+      results.add(all.get(i));
+      System.out.println("score" + all.get(i).getScore());
+    }
+    return results;
   }
+
+  private double getTermIDF(String term) {
+    int totalNum = _indexer.numDocs();
+    int containTermNum = _indexer.corpusDocFrequencyByTerm(term);
+    return Math.log((double)(totalNum) / (double)(containTermNum)); 
+  }
+  
 }
--- src/edu/nyu/cs/cs2580/RankerNumviews.java
+++ src/edu/nyu/cs/cs2580/RankerNumviews.java
@@ -1,5 +1,6 @@
 package edu.nyu.cs.cs2580;
 
+import java.util.Collections;
 import java.util.Vector;
 
 import edu.nyu.cs.cs2580.QueryHandler.CgiArguments;
@@ -22,7 +23,27 @@ public class RankerNumviews extends Ranker {
   @Override
   public Vector<ScoredDocument> runQuery(Query query, int numResults) {
     Vector<ScoredDocument> all = new Vector<ScoredDocument>();
-    // @CS2580: fill in your code here.
-    return all;
+    for (int i = 0; i < _indexer.numDocs(); ++i) {
+      all.add(scoreDocument(query, i));
+    }
+    Collections.sort(all, Collections.reverseOrder());
+    Vector<ScoredDocument> results = new Vector<ScoredDocument>();
+    for (int i = 0; i < all.size() && i < numResults; ++i) {
+      results.add(all.get(i));
+    }
+    return results;
+  }
+
+  private ScoredDocument scoreDocument(Query query, int index)
+  {
+      //Process raw query into tokens for display
+      query.processQuery();
+      
+      //Query independent
+      //Get the number of views of the document  
+      Document doc = _indexer.getDoc(index);
+      int score = doc.getNumViews();
+
+      return new ScoredDocument(query._query, doc, score);
   }
 }
